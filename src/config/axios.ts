9dcf59b62225c863c628d615f826f7654730374c
smy-101 <shimy1011@foxmail.com>
2021-01-16 14:47:50 +0800

    添加注册页面注册功能

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const appID = 'kdfrtgAEVJ1NhR9c8D9eb8Y3';
-const appSecret = '9A5GPDaTYomgui3FHc6ne2Mw';
+const appID = "kdfrtgAEVJ1NhR9c8D9eb8Y3"
+const appSecret = "9A5GPDaTYomgui3FHc6ne2Mw"
 
 const instance = axios.create({
     baseURL: 'https://gp-server.hunger-valley.com/',
@@ -11,30 +11,28 @@ const instance = axios.create({
     }
 });
 
-// 添加请求拦截器
+// Add a request interceptor
 instance.interceptors.request.use(function (config) {
-    // 在发送请求之前做些什么
-    const xToken = localStorage.getItem('x-token');
-    if (xToken) {
-        config.headers['Authorization'] = `Bearer ${xToken}`;
+    const xToken = localStorage.getItem('x-token')
+    if(xToken){
+        config.headers['Authorization'] = `Bearer ${xToken}`
     }
     return config;
 }, function (error) {
-    console.error(error);
-    // 对请求错误做些什么
+    console.error(error)
     return Promise.reject(error);
 });
 
-// 添加响应拦截器
+// Add a response interceptor
 instance.interceptors.response.use(function (response) {
-    // 对响应数据做点什么
+    // Do something with response data
     if(response.headers['x-token']){
         localStorage.setItem('x-token',response.headers['x-token'])
     }
     return response;
 }, function (error) {
-    // 对响应错误做点什么
+    // Do something with response error
     return Promise.reject(error);
 });
 
-export default instance;
+export default instance
--- src/views/SignUp.tsx
+++ src/views/SignUp.tsx
@@ -1,7 +1,69 @@
 import React from 'react';
+import {UserOutlined} from '@ant-design/icons';
+import {Input, Button} from 'antd';
+import axios from '../config/axios';
 
-function SignUp() {
-    return <h2>SignUp</h2>;
+interface Props {
+    account: string,
+    password: string,
+    passwordConformation: string
+}
+
+class SignUp extends React.Component<any, Props> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            account: '',
+            password: '',
+            passwordConformation: ''
+        };
+    }
+
+
+    onChangeAccount = (e: { target: { value: any; }; }) => {
+        this.setState({account: e.target.value});
+    };
+    onChangePassword = (e: { target: { value: any; }; }) => {
+        this.setState({password: e.target.value});
+    };
+    onChangePasswordConformation = (e: { target: { value: any; }; }) => {
+        this.setState({passwordConformation: e.target.value});
+    };
+    submit = async () => {
+        const { account,password,passwordConformation } = this.state;
+        try{
+            await axios.post('sign_up/user',{
+                account, // account: account
+                password,
+                password_confirmation:passwordConformation
+            })
+            console.log('success');
+        }catch(e){
+            throw new Error(e)
+        }
+    }
+
+
+    public render() {
+        const {account, password, passwordConformation} = this.state;
+        return (
+            <div>
+                <Input placeholder="请输入用户名" allowClear
+                       prefix={<UserOutlined/>}
+                       value={account}
+                       onChange={this.onChangeAccount}/>
+                <Input.Password
+                    value={password}
+                    onChange={this.onChangePassword}
+                    placeholder="请输入密码"/>
+                <Input.Password
+                    value={passwordConformation}
+                    onChange={this.onChangePasswordConformation}
+                    placeholder="请确认密码"/>
+                <Button onClick={this.submit}>注册</Button>
+            </div>
+        );
+    }
 }
 
 export {SignUp};
